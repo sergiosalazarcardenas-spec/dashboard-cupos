d249a21e205e4a769ba6340156ebe1c1af2f4219
sergiosalazarcardenas-spec <sergio.salazar.cardenas@gmail.com>
2026-08-05 15:57:01 -0400

    Conecta el dashboard a Google Sheets

---
+++ config.js
@@ -0,0 +1,7 @@
+/*
+  Pegue aquí la URL CSV publicada de la pestaña de Google Sheets que contiene
+  las columnas operativas. Deje vacío para trabajar sólo con data.js en local.
+*/
+window.DASHBOARD_CONFIG = {
+  googleSheetsCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQeY7BnsK6S8oKxOHFF-lLb-hHSd4DNHQ6EcKYBZzhNrEN3ubBBr0a3BaL3CT3Arw/pub?gid=1952857334&single=true&output=csv'
+};
